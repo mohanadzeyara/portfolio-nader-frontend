da7e6379dae5884a097f14e97d84e8e1ad0e7cf0
mohanadzeyara <mzeyara752.2@gmail.com>
2025-08-25 04:41:38 +0200

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import './styles.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
     <App />
-  </BrowserRouter>
+  </React.StrictMode>
 );
